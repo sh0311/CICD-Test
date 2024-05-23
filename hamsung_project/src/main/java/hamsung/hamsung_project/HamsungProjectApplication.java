26b72ad3e4c09b5b7b02b551ab1876b12e310b45
sh0311 <0311alice@naver.com>
2024-05-24 01:51:09 +0900

    초기설정 파일

---
+++ hamsung_project/src/main/java/hamsung/hamsung_project/HamsungProjectApplication.java
@@ -0,0 +1,13 @@
+package hamsung.hamsung_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HamsungProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HamsungProjectApplication.class, args);
+	}
+
+}
---
+++ hamsung_project/src/test/java/hamsung/hamsung_project/HamsungProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package hamsung.hamsung_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HamsungProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
